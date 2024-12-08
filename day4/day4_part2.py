732ca1cdc061385b885e36287b3396caa04f8659
Burak Aktas <aburakaktas@gmail.com>
2024-12-08 02:11:32 +0100

    Solved day4 part2. 💀

--- day4/day4_part2.py
+++ day4/day4_part2.py
@@ -12,21 +12,23 @@ def main():
     length_y = len(grid)
 
     directions = [
+        (-1, -1),  # up-left
+        (-1, 1),  # up-right
+        (1, -1),  # down-left
         (1, 1),  # down-right
-        (-1, -1), # up-left
-        (1, -1), # down-left
-        (-1, 1), # up-right
     ]
-    
+
     xmas_count = 0
     for i in range(length_y):
         for j in range(length_x):
-            if grid[i][j] == 'A':
+            if grid[i][j] == "A":
                 # first letter is A
-                for dx, dy in directions:
-                    word = ""
-                    new_i = i * dx
-                    new_j = j * dy
+
+                # check all 4 corners
+                word = ""
+                for x, y in directions:
+                    new_i = i + x
+                    new_j = j + y
 
                     # check if we don't go over grid
                     if boundary_check(new_i, new_j, length_x, length_y):
@@ -34,9 +36,8 @@ def main():
                     else:
                         # no need to continue this direction if we go out of the grid
                         break
-                    if word == "MSMS":
-                        xmas_count += 1
-                    
+                if word == "MSMS" or word == "SMSM" or word == "SSMM" or word == "MMSS":
+                    xmas_count += 1
 
     print(xmas_count)
 
@@ -45,7 +46,6 @@ def boundary_check(i, j, length_x, length_y):
     if 0 <= i < length_y and 0 <= j < length_x:
         return True
     return False
-
 
 
 if __name__ == "__main__":
